3db3f884bc777f9114652bd6516c796488a2f2db
Matt Thomas <matt@platformprojects.org>
2014-02-25 17:37:32 +0000

    Starting point for Ruby 3 exercises

---
+++ mortgage.rb
@@ -0,0 +1,7 @@
+def provide_mortgage?(salary, deposit, property_value)
+  loan_amount = property_value - deposit
+  min_deposit = 0.2 # 20%
+  max_multiplier = 5 # how many annual incomes can be borrowed
+  deposit >= property_value * min_deposit && 
+      salary * max_multiplier >= loan_amount
+end
